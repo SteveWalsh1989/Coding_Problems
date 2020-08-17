258fe1f0895fa7375b2f80de93079d457d31f2f1
Steve <stevewalsh89@gmail.com>
2020-08-17 15:56:22 +0100

    Quicksort added

--- sorting_quicksort.py
+++ sorting_quicksort.py
@@ -4,11 +4,51 @@ def quicksort(array):
     """ Quick Sort Algorithm"""
     print(f'Unsorted: {array}')
 
+    quicksort_helper(array, 0, len(array) - 1)
+
+    print(f'Sorted: {array}')
+
+    return array
 
 
-
-def quicksort_helper():
+def quicksort_helper(array, start_index, end_index):
     """helper function for quick sort"""
+    # base case
+    if start_index >= end_index:
+        return
+
+    # Set up pivot
+    pivot_index = start_index
+
+    # Set up left and right indexes
+    left_index = start_index + 1
+    right_index = end_index
+
+    # apply quick sort main logic
+    while right_index >= left_index:
+        if array[left_index] > array[right_index] and array[right_index] < array[pivot_index]:
+            swap(left_index, right_index, array)
+        if array[left_index] <= array[pivot_index]:
+            left_index += 1
+        if array[right_index] >= array[pivot_index]:
+            right_index -= 1
+
+    # swap pivot and right index
+    swap(pivot_index, right_index, array)
+
+    # For Space complexity work on smaller array first
+    left_subarray_is_smaller = right_index - 1 - start_index < end_index - (right_index + 1)
+    if left_subarray_is_smaller:
+        quicksort_helper(array, start_index, right_index - 1)
+        quicksort_helper(array, right_index + 1, end_index)
+    else:
+        quicksort_helper(array, right_index + 1, end_index)
+        quicksort_helper(array, start_index, right_index - 1)
+
+
+def swap(a, b, array):
+    """ swaps the elements of two positions within an array"""
+    array[a], array[b] = array[b], array[a]
 
 
 def main():
