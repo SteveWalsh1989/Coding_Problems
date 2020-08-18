08c31459f54d059083106115b711c308fa3b13d7
Steve <stevewalsh89@gmail.com>
2020-08-18 10:31:44 +0100

    palindrome check for words

--- easy/palindrome_check.py
+++ easy/palindrome_check.py
@@ -1,13 +1,28 @@
 
 
-def palindrome_check(word)
+def palindrome_check(word):
+    """Checks if a word is a palindrome"""
+
+    reversed_string = ""
+
+    for i in reversed(range(len(word))):
+        reversed_string += word[i]
+
+    is_palindrome = reversed_string == word
+
+    if is_palindrome:
+        print(f"'{word}' is a palindrome.")
+    else:
+        print(f"'{word}' is not a palindrome.")
 
 
 def main():
     """ Main function"""
-    wrd_1 = "racecar"
-    wrd_2 = "gigantic"
+    word_1 = "racecar"
+    word_2 = "gigantic"
 
+    palindrome_check(word_1)
+    palindrome_check(word_2)
 
 
 ''' Run Program '''
