a75a85691e951bded9339feb74a66507ff4ca602
Steve <stevewalsh89@gmail.com>
2020-08-18 10:37:33 +0100

    updated palindrome check to work with sentences

--- easy/palindrome_check.py
+++ easy/palindrome_check.py
@@ -1,28 +1,33 @@
 
 
-def palindrome_check(word):
-    """Checks if a word is a palindrome"""
+def palindrome_check(str):
+    """Checks if a word/sentence is a palindrome"""
+
+    # remove any white space for sentences
+    cln_str = str.replace(" ", "")
 
     reversed_string = ""
 
-    for i in reversed(range(len(word))):
-        reversed_string += word[i]
+    for i in reversed(range(len(cln_str))):
+        reversed_string += cln_str[i]
 
-    is_palindrome = reversed_string == word
+    is_palindrome = reversed_string == cln_str
 
     if is_palindrome:
-        print(f"'{word}' is a palindrome.")
+        print(f"'{cln_str}' is a palindrome.")
     else:
-        print(f"'{word}' is not a palindrome.")
+        print(f"'{cln_str}' is not a palindrome.")
 
 
 def main():
     """ Main function"""
-    word_1 = "racecar"
-    word_2 = "gigantic"
+    str_1 = "racecar"
+    str_2 = "gigantic"
+    str_3 = "borrow or rob"
 
-    palindrome_check(word_1)
-    palindrome_check(word_2)
+    palindrome_check(str_1)
+    palindrome_check(str_2)
+    palindrome_check(str_3)
 
 
 ''' Run Program '''
