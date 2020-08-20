c89c40ad4acd44a015b9b06834dff2f48bd3ec75
Steve <stevewalsh89@gmail.com>
2020-08-20 10:50:34 +0100

    added bst: finding closest value

---
+++ easy/bst_find_closest_value.py
@@ -0,0 +1,58 @@
+
+def find_closest_value(tree, target):
+    """ locates closet value within bst"""
+    return find_closest_value_helper(tree, target, tree.value)
+
+
+def find_closest_value_helper(tree, target, best_candidate):
+    """ helper function for recursion in locating best candidate"""
+
+    # for leaf nodes
+    if tree is None:
+        return best_candidate
+
+    # compare absolute difference between nodes and target
+    if abs(target - best_candidate) > abs(target - tree.value):
+        best_candidate = tree.value
+
+    # compare best candidate with target to determine best traversal route
+    if target > tree.value:
+        return find_closest_value_helper(tree.right, target, best_candidate)
+    elif target < tree.value:
+        return find_closest_value_helper(tree.left, target, best_candidate)
+    else:
+        return best_candidate 
+
+
+def main():
+    """ Main function"""
+
+    # create BST
+    root = BinaryTree(40)
+    root.left = BinaryTree(33)
+    root.left.left = BinaryTree(20)
+    root.left.left.left = BinaryTree(9)
+    root.left.right = BinaryTree(36)
+    root.right = BinaryTree(61)
+    root.right.left = BinaryTree(51)
+    root.right.left.right = BinaryTree(55)
+    root.right.right = BinaryTree(70)
+
+    value = 3
+    res = find_closest_value(root, value)
+
+    print(f" The closest value in the BST to {value} is {res}")
+
+
+# This is the class of the input binary tree.
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+''' Run Program '''
+main()
+
+
